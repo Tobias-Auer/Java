3280a9a084a62cc80c915b90f8fb4fdb6e137b15
Tobias Auer <tobias.auer@aol.com>
2023-05-11 21:59:00 +0200

    Worked on main class

--- Minecraft plugins/easyNewCraftingRecipes/src/main/java/de/tobias/easynewcraftingrecipes/EasyNewCraftingRecipes.java
+++ Minecraft plugins/easyNewCraftingRecipes/src/main/java/de/tobias/easynewcraftingrecipes/EasyNewCraftingRecipes.java
@@ -1,5 +1,6 @@
 package de.tobias.easynewcraftingrecipes;
 
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.Bukkit;
@@ -13,11 +14,15 @@ import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 public final class EasyNewCraftingRecipes extends JavaPlugin {
-    public ItemStack light;
-
-    public NamespacedKey nsKey;
+//    public ItemStack light;
+    List<NamespacedKey> nsKeyList = new ArrayList<>();
+    List<String> nsKeyListString = new ArrayList<>();
+    List<ItemStack> itemStackList = new ArrayList<>();
+    List<ShapedRecipe> shapedRecipeList = new ArrayList<>();
     @Override
     public void onEnable() {
         // Plugin startup logic
@@ -26,24 +31,35 @@ public final class EasyNewCraftingRecipes extends JavaPlugin {
             saveDefaultConfig();
         }
         YamlConfiguration config = YamlConfiguration.loadConfiguration(configFile);
+        ConfigurationSection rootSection = config.getConfigurationSection("");
 
-        light = new ItemStack(Material.LIGHT, 4);
-        nsKey = new NamespacedKey(this, "light_source");
+        if (rootSection != null) {
+            for (String key : rootSection.getKeys(false)) {
+                NamespacedKey namespacedKey = new NamespacedKey(this, key);
+//                String otherKeys = rootSection.getKeys(true).toString();
+                ItemStack itemStack = new ItemStack((Material) config.get(key+".item"), (Integer) config.get(key+".count"));
+                ShapedRecipe recipe = new ShapedRecipe(namespacedKey, itemStack);
+                recipe.shape((String) config.get(key+".top_row"), (String) config.get(key+".middle_row"), (String) config.get(key+".bottom_row"));
 
-        ShapedRecipe recipe = new ShapedRecipe(nsKey, light);
+// TODO: implement setIngedient functionality!!
+                
+                nsKeyList.add(namespacedKey);
+                nsKeyListString.add(key);
+                itemStackList.add(itemStack);
+            }
+        }
 
-        recipe.shape("***","*%*","***");
-
-        recipe.setIngredient('*', Material.GLASS_PANE);
-        recipe.setIngredient('%', Material.TORCH);
-
-        getServer().addRecipe(recipe);
+//        recipe.setIngredient('*', Material.GLASS_PANE);
+//        recipe.setIngredient('%', Material.TORCH);
+//        getServer().addRecipe(recipe);
     }
 
     public class onJoin implements Listener {
         @EventHandler
         public void onJoinEvent(PlayerJoinEvent event) {
-            event.getPlayer().discoverRecipe(nsKey);
+            for (NamespacedKey key: nsKeyList) {
+                event.getPlayer().discoverRecipe(key);
+            }
         }
     }
 
