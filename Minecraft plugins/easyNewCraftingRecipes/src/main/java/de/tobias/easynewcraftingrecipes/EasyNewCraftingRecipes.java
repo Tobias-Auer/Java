6503995d9de01832f6dbed1f1f99f5e959c06c0f
Tobias Auer <tobias.auer@aol.com>
2023-05-12 17:53:23 +0200

    Finished EasyNewCraftingRecipes

--- Minecraft plugins/easyNewCraftingRecipes/src/main/java/de/tobias/easynewcraftingrecipes/EasyNewCraftingRecipes.java
+++ Minecraft plugins/easyNewCraftingRecipes/src/main/java/de/tobias/easynewcraftingrecipes/EasyNewCraftingRecipes.java
@@ -1,6 +1,7 @@
 package de.tobias.easynewcraftingrecipes;
 
 import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.Bukkit;
@@ -14,44 +15,48 @@ import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
+import java.util.function.Supplier;
 
 public final class EasyNewCraftingRecipes extends JavaPlugin {
-//    public ItemStack light;
     List<NamespacedKey> nsKeyList = new ArrayList<>();
-    List<String> nsKeyListString = new ArrayList<>();
-    List<ItemStack> itemStackList = new ArrayList<>();
-    List<ShapedRecipe> shapedRecipeList = new ArrayList<>();
     @Override
     public void onEnable() {
-        // Plugin startup logic
         File configFile = new File(getDataFolder(), "config.yml");
         if (!configFile.exists()) {
             saveDefaultConfig();
         }
         YamlConfiguration config = YamlConfiguration.loadConfiguration(configFile);
-        ConfigurationSection rootSection = config.getConfigurationSection("");
 
-        if (rootSection != null) {
-            for (String key : rootSection.getKeys(false)) {
-                NamespacedKey namespacedKey = new NamespacedKey(this, key);
-//                String otherKeys = rootSection.getKeys(true).toString();
-                ItemStack itemStack = new ItemStack((Material) config.get(key+".item"), (Integer) config.get(key+".count"));
+        PluginManager pluginManager = Bukkit.getPluginManager();
+        pluginManager.registerEvents(new onJoin(), this);
+
+        for (String key : config.getKeys(false)) {
+            NamespacedKey namespacedKey = new NamespacedKey(this, key);
+            try {
+
+                getLogger().info("Adding new Crafting Recipe: " + namespacedKey);
+                ItemStack itemStack = new ItemStack(Material.getMaterial((String) config.get(key + ".item")), config.getInt(key + ".count"));
                 ShapedRecipe recipe = new ShapedRecipe(namespacedKey, itemStack);
-                recipe.shape((String) config.get(key+".top_row"), (String) config.get(key+".middle_row"), (String) config.get(key+".bottom_row"));
+                recipe.shape((String) config.get(key + ".top_row"), (String) config.get(key + ".middle_row"), (String) config.get(key + ".bottom_row"));
+                ConfigurationSection legendSection = config.getConfigurationSection(key + ".legend");
+                List<String> keyList = new ArrayList<>();
+                keyList.addAll(legendSection.getKeys(false));
+                for (String legendKey : keyList) {
+                    char keyChars = legendKey.charAt(0);
+                    Material value = Material.getMaterial((String) legendSection.get(legendKey));
+                    recipe.setIngredient(keyChars, value);
+                }
+                getServer().addRecipe(recipe);
 
-// TODO: implement setIngedient functionality!!
-                
                 nsKeyList.add(namespacedKey);
-                nsKeyListString.add(key);
-                itemStackList.add(itemStack);
+            } catch (Exception e) {
+                getLogger().info("Error in Crafting Recipe: " + namespacedKey);
             }
         }
-
-//        recipe.setIngredient('*', Material.GLASS_PANE);
-//        recipe.setIngredient('%', Material.TORCH);
-//        getServer().addRecipe(recipe);
     }
 
     public class onJoin implements Listener {
