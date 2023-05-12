71d1f056f6f62c824791e38716fdf943ed6f0cc7
Tobias Auer <tobias.auer@aol.com>
2023-05-12 18:15:10 +0200

    Add AutoShutdown Plugin

---
+++ Minecraft plugins/AutoShutdown/README.md
@@ -0,0 +1,5 @@
+# AutoShutdown
+This plugin enabley you to shutdown your server at a specific time
+
+# Important Note
+This Plugin is optimized for Windows and will NOT work on Linux or Mac!
---
+++ Minecraft plugins/AutoShutdown/src/main/java/de/tobias/autoshutdown/AutoShutdown.java
@@ -0,0 +1,17 @@
+package de.tobias.autoshutdown;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AutoShutdown extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
