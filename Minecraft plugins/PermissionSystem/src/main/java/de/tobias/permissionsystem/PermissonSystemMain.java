9dad6a52d409a6229c5ba06943167c10637c02b5
Tobias Auer <tobias.auer@aol.com>
2023-05-18 13:06:44 +0200

    Updated PermissionSystem

--- Minecraft plugins/PermissionSystem/src/main/java/de/tobias/permissionsystem/PermissonSystemMain.java
+++ Minecraft plugins/PermissionSystem/src/main/java/de/tobias/permissionsystem/PermissonSystemMain.java
@@ -7,11 +7,14 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
+import org.bukkit.permissions.Permission;
 import org.bukkit.permissions.PermissionAttachment;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 import java.util.UUID;
 
 import static org.bukkit.Bukkit.getLogger;
@@ -29,6 +32,7 @@ public class PermissonSystemMain implements CommandExecutor {
         this.plugin = plugin;
         this.attachmentManager = attachmentManager;
     }
+
     public void addPermission(UUID uuid, String permission) {
         try {
             List<String> existingList = config.getStringList(uuid.toString());
@@ -58,7 +62,7 @@ public class PermissonSystemMain implements CommandExecutor {
 
     public void removePermission(UUID uuid, String permission) {
         try {
-            List<String> existingList = config.getStringList(uuid + ".permissons");
+            List<String> existingList = config.getStringList(uuid.toString());
             existingList.remove(permission);
             config.set(uuid.toString(), existingList);
 
@@ -85,6 +89,10 @@ public class PermissonSystemMain implements CommandExecutor {
         }
     }
 
+    public boolean checkPermission(Player player, String permission) {
+        return player.isPermissionSet(permission);
+    }
+
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         Player player;
@@ -93,39 +101,72 @@ public class PermissonSystemMain implements CommandExecutor {
         }
 
         Player senderPlayer = (Player) sender;
-
-        if (args.length < 3) {
+        String action = args[0].toLowerCase();
+        if (args.length < 3 && !action.equals("removepermission")) {
             senderPlayer.sendMessage("Verwendung: /permission <add | remove> <player> <permission>");
             return true;
         }
 
-        String action = args[0].toLowerCase();
         player = Bukkit.getPlayer(args[1]);
         if (player == null) {
             sender.sendMessage("§4WARNING: §rPlayer not found.");
             return true;
         }
         String permission = args[2];
-
+        List<String> existingList = config.getStringList("permissions");
         switch (action) {
             case "add":
+                if (!existingList.contains(permission)) {
+                    existingList.add(permission);
+                    config.set("permissions", existingList);
+                    Permission permissionObj = new Permission(permission);
+                    Bukkit.getServer().getPluginManager().addPermission(permissionObj);
+                }
                 addPermission(player.getUniqueId(), permission);
                 senderPlayer.sendMessage("Berechtigung §b" + permission + " §rwurde hinzugefügt.");
                 break;
+
             case "remove":
-                removePermission(player.getUniqueId(), permission);
-                senderPlayer.sendMessage("Berechtigung §4" + permission + " §rwurde entfernt.");
+                if (checkPermission(player, permission)) {
+                    removePermission(player.getUniqueId(), permission);
+                    senderPlayer.sendMessage("Berechtigung §4" + permission + " §rwurde entfernt.");
+                }
+                else {
+                    senderPlayer.sendMessage("Berechtigung §4" + permission + " §wurde nicht gefunden.");
+                }
+
                 break;
+
+            case "removepermission":
+                if (existingList.contains(permission)) {
+                    existingList.remove(permission);
+                    config.set("permissions", existingList);
+                    for (String uuidString : config.getKeys(false)) {
+                        List<String> permissions = config.getStringList(uuidString);
+                        permissions.remove(permission);
+                        config.set(uuidString, permissions);
+                    }
+                    senderPlayer.sendMessage("Berechtigung §4" + permission + " §rwurde gelöscht und von allen Spielern entfernt!.");
+                } else {
+                    senderPlayer.sendMessage("Berechtigung §4" + permission + " §rwurde nicht gefunden.");
+                }
+                break;
+
             case "check":
-                    getLogger().info(String.valueOf(player.isPermissionSet("minecraft.command.gamemode")));
+                senderPlayer.sendMessage(String.valueOf(checkPermission(player, permission)));
                 break;
+
             default:
                 senderPlayer.sendMessage("Verwendung: /permission <add | remove> <player> <permission> Action: " + action);
                 break;
         }
-
+        try {
+            config.save(configFile);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
         return true;
     }
 
-
+// TODO MAke onenable, make removepermission command
 }
