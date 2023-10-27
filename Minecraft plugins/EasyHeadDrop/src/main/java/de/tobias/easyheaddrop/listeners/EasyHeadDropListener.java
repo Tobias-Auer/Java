08ca1fa5ab377ff7d5db5017506a9a241abde635
Tobias Auer <tobias.auer@aol.com>
2023-10-27 16:20:13 +0200

    Weapon name missing patch

--- Minecraft plugins/EasyHeadDrop/src/main/java/de/tobias/easyheaddrop/listeners/EasyHeadDropListener.java
+++ Minecraft plugins/EasyHeadDrop/src/main/java/de/tobias/easyheaddrop/listeners/EasyHeadDropListener.java
@@ -1,5 +1,6 @@
 package de.tobias.easyheaddrop.listeners;
 
+import org.apache.commons.lang.WordUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
@@ -14,47 +15,8 @@ import org.bukkit.inventory.meta.SkullMeta;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.Date;
-import java.util.Objects;
 
 public class EasyHeadDropListener implements Listener {
-    @EventHandler
-    public void onDeath(PlayerDeathEvent event) {
-        Player player = event.getEntity();
-        Player killer = player.getKiller();
-        String weapon;
-        try {
-             weapon = event.getEntity().getKiller().getInventory().getItemInMainHand().getItemMeta().getDisplayName();
-        } catch (Exception e) {
-             weapon = "-";
-    }
-
-
-        // Hier kannst du die Bedingungen für den Spieler einstellen, dessen Drop du bearbeiten möchtest
-        if (killer != null) {
-
-        // Erstelle den Spielerkopf
-        ItemStack playerHead = new ItemStack(Material.PLAYER_HEAD);
-        SkullMeta skullMeta = (SkullMeta) playerHead.getItemMeta();
-        assert skullMeta != null;
-        skullMeta.setOwningPlayer(Bukkit.getOfflinePlayer(player.getName()));
-
-        // Setze den Itemnamen
-        skullMeta.setDisplayName("§a" + player.getName() + "'s §7Head");
-
-        // Setze die Lores
-        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MMM/yyyy");
-        String currentDate = dateFormat.format(new Date());
-
-        String[] lores = getStrings(player, currentDate, weapon);
-        skullMeta.setLore(Arrays.asList(lores));
-
-        playerHead.setItemMeta(skullMeta);
-
-        // Füge den Spielerkopf zum Drop hinzu
-        event.getDrops().add(playerHead);
-        }
-    }
-
     private static String[] getStrings(Player player, String currentDate, String weapon) {
         String killerName = "";
         EntityDamageEvent damageEvent = player.getLastDamageCause();
@@ -66,12 +28,60 @@ public class EasyHeadDropListener implements Listener {
             }
         }
 
-        String[] lores = new String[] {
-                "§eDied on: §f" + currentDate,
-                "§eKilled by: §f§l" + killerName,
-                "§eKilled with: §f§l" + weapon
-        };
-        return lores;
+        return new String[]{"§eDied on: §f" + currentDate, "§eKilled by: §f§l" + killerName, "§eKilled with: §f§l" + weapon};
     }
 
+    @EventHandler
+    public void onDeath(PlayerDeathEvent event) {
+        Player player = event.getEntity();
+        Player killer = player.getKiller();
+        String weapon;
+        try {
+            assert killer != null;
+            ItemStack murderWeapon = killer.getInventory().getItemInMainHand();
+            String murderWeaponName;
+            if (murderWeapon.getType() == Material.AIR) {
+                murderWeaponName = "Fist";
+            } else if (!murderWeapon.hasItemMeta() || murderWeapon.getItemMeta().getDisplayName().equals(""))
+                murderWeaponName = capitalizeMultiWordMaterialString(murderWeapon.getType().toString().toLowerCase());
+            else murderWeaponName = murderWeapon.getItemMeta().getDisplayName();
+            weapon = murderWeaponName;
+        } catch (Exception e) {
+            weapon = "-";
+        }
+        // Hier kannst du die Bedingungen für den Spieler einstellen, dessen Drop du bearbeiten möchtest
+        if (killer != null) {
+            // Erstelle den Spielerkopf
+            ItemStack playerHead = new ItemStack(Material.PLAYER_HEAD);
+            SkullMeta skullMeta = (SkullMeta) playerHead.getItemMeta();
+            assert skullMeta != null;
+            skullMeta.setOwningPlayer(Bukkit.getOfflinePlayer(player.getName()));
+
+            // Setze den Itemnamen
+            skullMeta.setDisplayName("§a" + player.getName() + "'s §7Head");
+
+            // Setze die Lores
+            SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MMM/yyyy");
+            String currentDate = dateFormat.format(new Date());
+
+            String[] lores = getStrings(player, currentDate, weapon);
+            skullMeta.setLore(Arrays.asList(lores));
+
+            playerHead.setItemMeta(skullMeta);
+
+            // Füge den Spielerkopf zum Drop hinzu
+            event.getDrops().add(playerHead);
+        }
+    }
+
+    private String capitalizeMultiWordMaterialString(String string) {
+        StringBuilder stringBuilder = new StringBuilder();
+        String[] tokens = string.split("_");
+        for (int i = 0; i < tokens.length; ++i)
+            stringBuilder.append(WordUtils.capitalize(tokens[i]) + (i == (tokens.length - 1) ? "" : " "));
+        return stringBuilder.toString();
+    }
 }
+
+
+
